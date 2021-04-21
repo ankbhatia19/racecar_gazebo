acb2736eee190c0e7391f9c38fac5fcbf45ee25b
devanshdhrafani <f20171569@hyderabad.bits-pilani.ac.in>
2021-04-21 10:15:24 +0000

    Fixed launch errors, migrated python script.

--- racecar_control/racecar_control/servo_commands.py
+++ racecar_control/racecar_control/servo_commands.py
@@ -1,5 +1,8 @@
-#!/usr/bin/env python
-import rospy
+#!/usr/bin/env python3
+import rclpy
+import sys
+from rclpy.qos import qos_profile_sensor_data
+from rclpy.node import Node
 from std_msgs.msg import Bool
 from std_msgs.msg import Float32
 from std_msgs.msg import Float64
@@ -11,13 +14,13 @@ def set_throttle_steer(data):
 
     global flag_move
 
-    pub_vel_left_rear_wheel = rospy.Publisher('/racecar/left_rear_wheel_velocity_controller/command', Float64, queue_size=1)
-    pub_vel_right_rear_wheel = rospy.Publisher('/racecar/right_rear_wheel_velocity_controller/command', Float64, queue_size=1)
-    pub_vel_left_front_wheel = rospy.Publisher('/racecar/left_front_wheel_velocity_controller/command', Float64, queue_size=1)
-    pub_vel_right_front_wheel = rospy.Publisher('/racecar/right_front_wheel_velocity_controller/command', Float64, queue_size=1)
+    pub_vel_left_rear_wheel = node.create_publisher(Float64,'/racecar/left_rear_wheel_velocity_controller/command', queue_size=1)
+    pub_vel_right_rear_wheel = node.create_publisher(Float64,'/racecar/right_rear_wheel_velocity_controller/command', queue_size=1)
+    pub_vel_left_front_wheel = node.create_publisher(Float64,'/racecar/left_front_wheel_velocity_controller/command', queue_size=1)
+    pub_vel_right_front_wheel = node.create_publisher(Float64,'/racecar/right_front_wheel_velocity_controller/command', queue_size=1)
 
-    pub_pos_left_steering_hinge = rospy.Publisher('/racecar/left_steering_hinge_position_controller/command', Float64, queue_size=1)
-    pub_pos_right_steering_hinge = rospy.Publisher('/racecar/right_steering_hinge_position_controller/command', Float64, queue_size=1)
+    pub_pos_left_steering_hinge = node.create_publisher(Float64,'/racecar/left_steering_hinge_position_controller/command', queue_size=1)
+    pub_pos_right_steering_hinge = node.create_publisher(Float64,'/racecar/right_steering_hinge_position_controller/command', queue_size=1)
 
     throttle = data.drive.speed/0.1
     steer = data.drive.steering_angle
@@ -29,17 +32,19 @@ def set_throttle_steer(data):
     pub_pos_left_steering_hinge.publish(steer)
     pub_pos_right_steering_hinge.publish(steer)
 
-def servo_commands():
+def servo_commands(args=sys.argv):
 
-    rospy.init_node('servo_commands', anonymous=True)
+    rclpy.init(args=sys.argv)
+    node = rclpy.create_node('servo_commands')
+    node.get_logger().info('Created node')
 
-    rospy.Subscriber("/racecar/ackermann_cmd_mux/output", AckermannDriveStamped, set_throttle_steer)
+    node.create_subscription(AckermannDriveStamped,"/racecar/ackermann_cmd_mux/output", set_throttle_steer,qos_profile=qos_profile_sensor_data)
 
     # spin() simply keeps python from exiting until this node is stopped
-    rospy.spin()
+    rclpy.spin(node)
 
 if __name__ == '__main__':
     try:
         servo_commands()
-    except rospy.ROSInterruptException:
+    except:
         pass
--- racecar_control/setup.py
+++ racecar_control/setup.py
@@ -1,3 +1,5 @@
+import os
+from glob import glob
 from setuptools import setup
 
 package_name = 'racecar_control'
@@ -10,6 +12,8 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.xml')),
+        (os.path.join('share', package_name, 'config'), glob('config/*.yaml'))
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -20,6 +24,8 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'keyop = racecar_control.keyboard_teleop:main',
+            'servo_commands = racecar_control.servo_commands:servo_commands'
         ],
     },
 )
