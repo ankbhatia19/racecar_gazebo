19aeaa0c74d3ccf33c56488667c273cc61e0b8a2
devanshdhrafani <f20171569@hyderabad.bits-pilani.ac.in>
2021-04-25 06:37:17 +0000

    Conv to pure py. Copy URDF/Meshes to share dir.

---
+++ racecar_description/setup.py
@@ -0,0 +1,32 @@
+import os
+from glob import glob
+from setuptools import setup
+
+package_name = 'racecar_description'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'meshes'), glob('meshes/*.dae')),
+        (os.path.join('share', package_name, 'meshes'), glob('meshes/*.STL')),
+        (os.path.join('share', package_name, 'models'), glob('models/*')),
+        (os.path.join('share', package_name, 'urdf'), glob('urdf/*'))
+        
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='root',
+    maintainer_email='f20171569@hyderabad.bits-pilani.ac.in',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
