3977ee2803766c08191fc6ce354b3572a1fcc886
devanshdhrafani <f20171569@hyderabad.bits-pilani.ac.in>
2021-06-01 03:06:56 +0000

    Mesh files are now copied to install directory.

--- racecar_description/setup.py
+++ racecar_description/setup.py
@@ -12,8 +12,10 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name, 'meshes'), glob('/meshes')),
-        (os.path.join('share', package_name, 'urdf'), glob('urdf/*'))
+        (os.path.join('share', package_name, 'meshes'), glob('meshes/*.dae')),
+        (os.path.join('share', package_name, 'urdf'), glob('urdf/*.xml')),
+        (os.path.join('share', package_name, 'urdf'), glob('urdf/*.gazebo')),
+
         
     ],
     install_requires=['setuptools'],
