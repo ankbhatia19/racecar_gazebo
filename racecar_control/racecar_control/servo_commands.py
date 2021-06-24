8fa2021887fccdaf61fedb179afa067760733339
jacobjj <jjj025@ucsd.edu>
2021-06-23 23:46:58 -0700

    publish control messages

--- racecar_control/racecar_control/servo_commands.py
+++ racecar_control/racecar_control/servo_commands.py
@@ -3,9 +3,7 @@ import rclpy
 import sys
 from rclpy.qos import qos_profile_sensor_data
 from rclpy.node import Node
-from std_msgs.msg import Bool
-from std_msgs.msg import Float32
-from std_msgs.msg import Float64
+from std_msgs.msg import Float64MultiArray
 from ackermann_msgs.msg import AckermannDriveStamped
 
 flag_move = 0
@@ -14,13 +12,14 @@ rclpy.init(args=sys.argv)
 node = rclpy.create_node('servo_commands')
 node.get_logger().info('Created node')
 
-pub_vel_left_rear_wheel = node.create_publisher(Float64,'/racecar/left_rear_wheel_velocity_controller/command',5)
-pub_vel_right_rear_wheel = node.create_publisher(Float64,'/racecar/right_rear_wheel_velocity_controller/command',5)
-pub_vel_left_front_wheel = node.create_publisher(Float64,'/racecar/left_front_wheel_velocity_controller/command',5)
-pub_vel_right_front_wheel = node.create_publisher(Float64,'/racecar/right_front_wheel_velocity_controller/command',5)
+pub_vel_left_rear_wheel = node.create_publisher(Float64MultiArray,'/left_rear_wheel_velocity_controller/commands',5)
+pub_vel_right_rear_wheel = node.create_publisher(Float64MultiArray,'/right_rear_wheel_velocity_controller/commands',5)
+pub_vel_left_front_wheel = node.create_publisher(Float64MultiArray,'/left_front_wheel_velocity_controller/commands',5)
+pub_vel_right_front_wheel = node.create_publisher(Float64MultiArray,'/right_front_wheel_velocity_controller/commands',5)
 
-pub_pos_left_steering_hinge = node.create_publisher(Float64,'/racecar/left_steering_hinge_position_controller/command',5)
-pub_pos_right_steering_hinge = node.create_publisher(Float64,'/racecar/right_steering_hinge_position_controller/command',5)
+# TODO: This still is not connected to the controller
+pub_pos_left_steering_hinge = node.create_publisher(Float64MultiArray,'/left_steering_hinge_position_controller/commands',5)
+pub_pos_right_steering_hinge = node.create_publisher(Float64MultiArray,'/right_steering_hinge_position_controller/commands',5)
 
 
 def set_throttle_steer(data):
@@ -29,12 +28,13 @@ def set_throttle_steer(data):
     throttle = data.drive.speed/0.1
     steer = data.drive.steering_angle
 
-    pub_vel_left_rear_wheel.publish(Float64(data=throttle))
-    pub_vel_right_rear_wheel.publish(Float64(data=throttle))
-    pub_vel_left_front_wheel.publish(Float64(data=throttle))
-    pub_vel_right_front_wheel.publish(Float64(data=throttle))
-    pub_pos_left_steering_hinge.publish(Float64(data=steer))
-    pub_pos_right_steering_hinge.publish(Float64(data=steer))
+    pub_vel_left_rear_wheel.publish(Float64MultiArray(data=[throttle]))
+    pub_vel_right_rear_wheel.publish(Float64MultiArray(data=[throttle]))
+    pub_vel_left_front_wheel.publish(Float64MultiArray(data=[throttle]))
+    pub_vel_right_front_wheel.publish(Float64MultiArray(data=[throttle]))
+    
+    pub_pos_left_steering_hinge.publish(Float64MultiArray(data=[steer]))
+    pub_pos_right_steering_hinge.publish(Float64MultiArray(data=[steer]))
 
 def servo_commands(args=sys.argv):
 
